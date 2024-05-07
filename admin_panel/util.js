ebaa138eecf638e6a2cbf729ee20ad69eb92ab4b
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-07 19:35:37 +0530

    Add files via upload

---
+++ admin_panel/util.js
@@ -0,0 +1,36 @@
+import Dashboard from './assets/Dashboard.png';
+import Cart from './assets/Cart.png';
+import star from './assets/Empty_Star.png';
+import Product from './assets/Product.png';
+import Settings from './assets/Settings.png';
+import Users from './assets/Users.png';
+
+export const left_menu_list = [
+    {
+    name : "Dashboard",
+    image : Dashboard
+    },
+    {
+    name : "Products",
+    image : Product
+    },
+    {
+    name : "Orders",
+    image : Cart
+    },
+    {
+    name : "Customers",
+    image : Users
+    },
+    {
+    name : "Reviews",
+    image : star
+    },
+    {
+    name : "Settings",
+    image : Settings
+    }
+    ];
+    
+    // export {left_menu_list};
+    
