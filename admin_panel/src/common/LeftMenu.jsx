34dc762b9cb8d310c498a6f70247a6128e43d63e
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-07 19:31:38 +0530

    Update LeftMenu.jsx

--- admin_panel/src/common/LeftMenu.jsx
+++ admin_panel/src/common/LeftMenu.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {left_menu_list} from '../../util';
 
 export const LeftMenu = () => {
   return (
-    <div>LeftMenu</div>
+    <nav className='h-full text-black w-64'>
+      <ol className=' flex flex-col gap-y-5'>
+        {
+          left_menu_list.map((v,i) => {
+            return(
+              <li key={`left_menu_${v.name}`} className='flex gap-x-3'>
+                <img src={v.image} alt={`left_menu_image_${v.name}`} />
+                {v.name}
+              </li>
+            )
+          })
+        }
+      </ol>
+    </nav>
   )
 }
