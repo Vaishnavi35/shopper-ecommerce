5c29d3133cdee08521551012cdc019856abb956d
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-08 19:08:43 +0530

    Update App.jsx

--- admin_panel/src/App.jsx
+++ admin_panel/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-
 import "./index.scss";
 import { LeftMenu } from "./common/LeftMenu";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 const App = () => (
- <LeftMenu></LeftMenu>
+    <>
+        <LeftMenu></LeftMenu>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Dashboard />}/>
+            </Routes>
+        </BrowserRouter>
+    </>
 );
 
 createRoot(document.getElementById("app")).render(<App />);
