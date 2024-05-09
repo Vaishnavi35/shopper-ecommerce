f58010ceba74ee31dee0c8df0ae7357f94e0a040
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-09 19:47:09 +0530

    Tailwind prefix changes

--- admin_panel/src/common/LeftMenu.jsx
+++ admin_panel/src/common/LeftMenu.jsx
@@ -13,15 +13,15 @@ export const LeftMenu = () => {
 
   return (
     <div>
-      <div className='h-full text-black w-64 py-5  text-center shadow-md sidebar fixed left-0 z-10 bg-white'>
-        <div className='px-5'>
+      <div className='tw-h-full  tw-w-64 tw-py-5  tw-text-center tw-shadow-md sidebar tw-fixed tw-left-0 tw-z-10 tw-bg-white'>
+        <div className='tw-px-5'>
           shopper
         </div>
-        <ol className='flex flex-col gap-y-5 justify-center my-12 px-5'>
+        <ol className='tw-flex tw-flex-col tw-gap-y-5 tw-justify-center tw-my-12 tw-px-5'>
           {
             left_menu_list.map((v,i) => {
               return( 
-                <li key={`left_menu_${v.name}`} className='flex gap-x-3 h-10 w-full grey_color items-center px-6 rounded-md' onClick={() => leftMenuFn(v.name)}>
+                <li key={`left_menu_${v.name}`} className='tw-flex tw-gap-x-3 tw-h-10 tw-w-full grey_color tw-items-center tw-px-6 tw-rounded-md' onClick={() => leftMenuFn(v.name)}>
                   <img src={v.image} alt={`left_menu_image_${v.name}`}/>
                   {v.name}
                 </li>
@@ -29,22 +29,21 @@ export const LeftMenu = () => {
             })
           }
         </ol>
-        <div className='border-[#E9E9EB] border'></div>
-        <div className='px-5'>
-          <li className='flex gap-x-3 h-10 w-full grey_color items-center px-6 rounded-md mt-12' onClick={() => leftMenuFn("Extras")}>
+        <div className='tw-border-[#E9E9EB] tw-border'></div>
+        <div className='tw-px-5'>
+          <li className='tw-flex tw-gap-x-3 tw-h-10 tw-w-full grey_color tw-items-center tw-px-6 tw-rounded-md tw-mt-12' onClick={() => leftMenuFn("Extras")}>
             <img src={Add} alt="extra_add_icon"/>
             Extras
           </li>
         </div>
-        
       </div>
-      <nav className='flex justify-between px-10 fixed top-0 left-64 h-[72px] items-center shadow-md z-10 bg-white'>
-        <div className='flex gap-x-1'>
+      <nav className='tw-flex tw-justify-between tw-px-10 tw-fixed tw-top-0 tw-left-64 tw-h-[72px] tw-items-center tw-shadow-md tw-z-10 tw-bg-white'>
+        <div className='tw-flex tw-gap-x-1 tw-text-black'>
           <span className='grey_color'>Admin</span> 
           <img src={Right} alt="Right" />
           {leftMenu}
         </div>
-        <img src={Logout} alt="Logout" className='cursor-pointer'/>
+        <img src={Logout} alt="Logout" className='tw-cursor-pointer'/>
       </nav>
     </div>
   )
