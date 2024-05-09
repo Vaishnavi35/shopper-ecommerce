13440b20042e4c29d24259374c5191ce33bfe73c
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-09 19:46:49 +0530

    Customer & sales completion

--- admin_panel/src/components/Dashboard.jsx
+++ admin_panel/src/components/Dashboard.jsx
@@ -1,8 +1,24 @@
 import { Chart, registerables } from 'chart.js';
-import { Line, Bar } from "react-chartjs-2";
+import {  Line,Bar } from "react-chartjs-2";
+import ProgressBar from "react-bootstrap/ProgressBar";
+// import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+
+  
+
 
 const Dashboard = () => {
     Chart.register(...registerables);
+    const orders = 734;
+    const orders_left = 266;
+    const total_sales = 2400;
+    const Rdata = [
+        { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Page B', uv: 300, pv: 1398, amt: 2210 },
+        { name: 'Page C', uv: 200, pv: 9800, amt: 2290 },
+        { name: 'Page D', uv: 278, pv: 3908, amt: 2000 },
+        { name: 'Page E', uv: 189, pv: 4800, amt: 2181 },
+        // Add more records as needed
+      ];
     const labels = [
         'day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7', 'day8', 'day9', 'day10',
         'day11', 'day12', 'day13', 'day14', 'day15', 'day16', 'day17', 'day18', 'day19', 'day20',
@@ -11,10 +27,19 @@ const Dashboard = () => {
 
     const options = {
         plugins: {
-            
+            legend: {
+                display: true,
+                position: 'top'
+            },
             title: {
                 display: true,
                 text: 'Total Sales\nTHIS MONTH\n $54546',
+                // font: {
+                //     size: 16
+                // },
+                callback: (context) => {
+                    return 'Total Sales\nTHIS MONTH\n $54546';
+                }
             }
         },
         scales: {
@@ -30,7 +55,7 @@ const Dashboard = () => {
         labels : labels,
         datasets : [
             {
-                label : "Sales",
+                label : "Weekly report",
                 backgroundColor: '#4078FF',
                 borderColor: '#4078FF',
                 borderWidth: 1,
@@ -47,7 +72,7 @@ const Dashboard = () => {
             labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5'],
             datasets: [
               {
-                label: 'Customers',
+                label: '',
                 data: [65, 59, 80, 81, 56, 55, 40],
                 fill: false,
                 borderColor: '#4078FF',
@@ -59,11 +84,7 @@ const Dashboard = () => {
     const lineOptions = {
         plugins: {
             title: {
-              display: true,
-              text: 'Customers\nTHIS MONTH\n 54546', // Title text
-              font: {
-                size: 20 // Title font size
-              }
+              display: false,
             }
           },
           scales: {
@@ -73,29 +94,88 @@ const Dashboard = () => {
           }
     };
 
+    const best_selling = [
+        {
+            name: "Classic Monochrome Tees",
+            amount : 940
+        },
+        {
+            name: "Monochromatic Wardrobe",
+            amount : 790
+        },
+        {
+            name: "Essential Neutrals",
+            amount : 740
+        }
+    ];
+
     return(
-        <div className="p-10 grid gap-10 dashboard justify-center items-center grid-cols-3"  style={{ gridTemplateRows: '30% auto' }}>
-            <div className="w-full border-[#E9E9EB] border-2 rounded-lg p-5 h-full">
+        <div className="tw-p-10 tw-grid tw-gap-10 dashboard tw-justify-center tw-items-center tw-grid-cols-3 tw-bg-[#F6F6F6]" style={{ gridTemplateRows: '30% auto' }}>
+            <div className="tw-w-full tw-border-[#E9E9EB] tw-border-2 tw-rounded-lg tw-p-5 tw-h-full tw-bg-white">
                 <div style={{width: '100%', height: '100%'}}>
                     <Bar data={data} options={options}/>
                 </div>
             </div>
-            <div>
-                <div className="w-full border-[#E9E9EB] border-2 rounded-lg p-5 h-full">
-                    <div style={{width: '100%', height: '100%'}}>
-                        <Line data={lineData} options={lineOptions}/>
+            {/* <div> */}
+                <div className="tw-w-full tw-border-[#E9E9EB] tw-border-2 tw-rounded-lg tw-p-5 tw-h-full tw-bg-white">
+                    <div className='tw-grid' style={{ gridTemplateRows: '40px minmax(0, 1fr)' }}>
+                        <div className=' tw-grid tw-grid-cols-2 tw-justify-between'>
+                            <div>
+                                <div className='tw-text-base tw-font-semibold'>Customers</div>
+                                <div className='grey_color tw-text-xs tw-font-medium'>THIS MONTH</div>
+                            </div>
+                            <div className=' tw-text-right tw-text-2xl tw-font-bold'>{orders.toLocaleString()}</div>
+                        </div>
+                        <div style={{ width: '100%', height: '100%' }}>
+                            {/* <ResponsiveContainer width='100%' aspect={4.0/3.0}>
+                        <LineChart  data={Rdata}>
+                            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                            <CartesianGrid stroke="#ccc" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                        </LineChart>
+                        </ResponsiveContainer> */}
+                            <Line data={lineData} options={lineOptions}/>
+                        </div>
+                    </div>
+                </div>
+            {/* </div> */}
+            {/* <div> */}
+                <div className="tw-w-full tw-border-[#E9E9EB] tw-border-2 tw-rounded-lg tw-p-5 tw-h-full tw-bg-white">
+                    <div style={{width: '100%', height: '100%'}} className='tw-grid tw-grid-rows-[auto,auto,5px]'>
+                        <div className=' tw-grid tw-grid-cols-2 tw-justify-between'>
+                            <div>
+                                <div className='tw-text-base tw-font-semibold'>Orders</div>
+                                <div className='grey_color tw-text-xs tw-font-medium'>MONTHLY GOALS : 1,000</div>
+                            </div>
+                            <div className=' tw-text-right tw-text-2xl tw-font-bold'>{orders}</div>
+                        </div>
+                        <div className='grey_color tw-text-xs tw-self-end tw-mb-1 tw-font-medium'>{orders_left} Left</div>
+                        <ProgressBar animated now={orders} className='tw-h-2 tw-self-end ' max={1000} min={0}/>
+                    </div>
+                </div>
+            {/* </div> */}
+            <div className='tw-w-full tw-border-[#E9E9EB] tw-border-2 tw-rounded-lg tw-h-full tw-bg-white tw-grid' style={{ gridTemplateRows: '78px minmax(0, 1fr)' }}>
+                <div className='tw-border-[#E9E9EB] tw-border-b tw-p-5 '>
+                    <div className='tw-text-base tw-font-semibold tw-mb-2'>Best Selling</div>
+                    <div className='grey_color tw-text-xs tw-font-medium'>THIS MONTH</div>
+                </div>
+                <div className='tw-p-5 '>
+                    <div className='tw-text-2xl tw-font-bold tw-mb-3'>${total_sales.toLocaleString()} <span className='grey_color tw-text-sm tw-font-medium'>-- Total Sales</span></div>
+                    {
+                        best_selling.map((v,i) => {
+                            return(
+                                <div key={`best_selling_${v.name}`} className='border-[#E6E7E8] tw-mb-3 border tw-rounded-full tw-text-xs tw-text-center tw-h-6 grey_color tw-flex tw-gap-1 tw-justify-center tw-items-center'>{v.name} —-  <span className='tw-text-black'>${v.amount.toLocaleString()} Sales</span></div>
+                            )
+                        })
+                    }
+                    <div>
+
                     </div>
                 </div>
             </div>
-            <div>
-                <div className="w-full border-[#E9E9EB] border-2 rounded-lg p-5 h-full">
-                    <div style={{width: '100%', height: '100%'}}>
-                        <Bar data={data} options={options}/>
-                    </div>
-                </div>
-            </div>
-            <div>4</div>
-            <div className='col-span-2'>5</div>
+            <div className='tw-col-span-2 tw-bg-white'>5</div>
         </div>
     )
 }
