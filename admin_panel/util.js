cdfcc198639a4b8a5b7f98f5fc248415a464a838
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-10 19:24:29 +0530

    order data table records

--- admin_panel/util.js
+++ admin_panel/util.js
@@ -4,6 +4,11 @@ import star from './assets/Empty_Star.png';
 import Product from './assets/Product.png';
 import Settings from './assets/Settings.png';
 import Users from './assets/Users.png';
+const moment = require('moment');
+
+function changeDateFormat(dateString) {
+    return moment(dateString).format('DD MMM, YYYY');
+}
 
 export const left_menu_list = [
     {
@@ -30,7 +35,40 @@ export const left_menu_list = [
     name : "Settings",
     image : Settings
     }
-    ];
+];
+
+export const ordered_products = [
+    {
+        "item": "Laptop",
+        "date": changeDateFormat("2024-05-10"),
+        "amount": "$1200.00",
+        "status": "Delivered"
+      },
+      {
+        "item": "Smartphone",
+        "date": changeDateFormat("2024-05-09"),
+        "amount": "$800.00",
+        "status": "Processing"
+      },
+      {
+        "item": "Headphones",
+        "date": changeDateFormat("2024-05-08"),
+        "amount": "$150.00",
+        "status": "Pending"
+      },
+      {
+        "item": "Tablet",
+        "date": changeDateFormat("2024-05-07"),
+        "amount": "$500.00",
+        "status": "Delivered"
+      },
+      {
+        "item": "Smartwatch",
+        "date": changeDateFormat("2024-05-06"),
+        "amount": "$300.00",
+        "status": "Delivered"
+      }
+];
     
     // export {left_menu_list};
     
