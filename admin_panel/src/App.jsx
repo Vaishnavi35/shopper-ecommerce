b6427fa8673daefe0d0ccfded8f15e1cf001e53f
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-10 19:26:59 +0530

    prime react import changes

--- admin_panel/src/App.jsx
+++ admin_panel/src/App.jsx
@@ -4,15 +4,25 @@ import "./index.scss";
 import { LeftMenu } from "./common/LeftMenu";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+// import 'primereact/resources/themes/saga-blue/theme.css'; // Theme
+// import 'primereact/resources/primereact.min.css'; 
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
+
 
 const App = () => (
     <>
-        <LeftMenu></LeftMenu>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Dashboard />}/>
-            </Routes>
-        </BrowserRouter>
+        <PrimeReactProvider>
+            <LeftMenu></LeftMenu>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Dashboard />}/>
+                </Routes>
+            </BrowserRouter>
+        </PrimeReactProvider>
     </>
 );
 
