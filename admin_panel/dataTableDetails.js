ac18eaca6f3ac3b9516f78ae4c26fac79b909792
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-13 19:39:45 +0530

    attributes & category columns & actions

--- admin_panel/dataTableDetails.js
+++ admin_panel/dataTableDetails.js
@@ -29,13 +29,13 @@ export const dataTableDetails = {
       actions: [
         {
           label: 'Change Status',
-          action: 'change_status',
-          command: (event) => handleActionClick('change_status')
+          action: 'order_change_status',
+          command: (event) => handleActionClick('order_change_status')
         },
         {
           label: 'View Details',
-          action: 'view_details',
-          command: (event) => handleActionClick('view_details')
+          action: 'order_view_details',
+          command: (event) => handleActionClick('order_view_details')
         }
       ]
     },
@@ -69,19 +69,159 @@ export const dataTableDetails = {
       actions: [
         {
           label: 'Edit',
-          action: 'Edit',
-          command: (event) => handleActionClick('Edit')
+          action: 'product_edit',
+          command: (event) => handleActionClick('product_edit')
         },
         {
           label: 'Delete',
-          action: 'Delete',
-          command: (event) => handleActionClick('Delete')
+          action: 'product_delete',
+          command: (event) => handleActionClick('product_delete')
         },
         {
           label: 'View',
-          action: 'View',
-          command: (event) => handleActionClick('View')
+          action: 'product_view',
+          command: (event) => handleActionClick('product_view')
         }
+      ]
+    },
+    customers: {
+      columns: [
+        {
+          field: "image",
+          name: "Image"
+        },
+        {
+          field: "customer_name",
+          name: "Name"
+        },
+        {
+          field: "customer_email",
+          name: "Email"
+        },
+        {
+          field: "customer_address",
+          name: "Shipping Address"
+        },
+        {
+          field: "",
+          name: "Action"
+        }
+      ],
+      actions: [
+        {
+          label: 'Edit',
+          action: 'customer_edit',
+          command: (event) => handleActionClick('customer_edit')
+        },
+      ]
+    },
+    reviews : {
+      columns: [
+        {
+          field: "image",
+          name: "Image"
+        },
+        {
+          field: "customer_name",
+          name: "Name"
+        },
+        {
+          field: "review",
+          name: "Review"
+        },
+        {
+          field: "review_rate",
+          name: "Rating"
+        },
+        {
+          field: "review_submitted_at",
+          name: "Submitted at"
+        },
+        {
+          field: "",
+          name: "Action"
+        }
+      ],
+      actions: [
+        {
+          label: 'Reply',
+          action: 'reply_review',
+          command: (event) => handleActionClick('reply_review')
+        },
+        {
+          label: 'Delete',
+          action: 'delete_review',
+          command: (event) => handleActionClick('delete_review')
+        },
+        {
+          label: 'View',
+          action: 'view_review',
+          command: (event) => handleActionClick('view_review')
+        }
+      ]
+    },
+    categories : {
+      columns: [
+        {
+          field: "image",
+          name: "Image"
+        },
+        {
+          field: "category_name",
+          name: "Name"
+        },
+        {
+          field: "category_description",
+          name: "Description"
+        },
+        {
+          field: "",
+          name: "Action"
+        }
+      ],
+      actions: [
+        {
+          label: 'Edit',
+          action: 'edit_category',
+          command: (event) => handleActionClick('edit_category')
+        },
+        {
+          label: 'Delete',
+          action: 'delete_category',
+          command: (event) => handleActionClick('delete_category')
+        },
+      ]
+    },
+    attributes : {
+      columns: [
+        {
+          field: "image",
+          name: "Image"
+        },
+        {
+          field: "attribute_name",
+          name: "Name"
+        },
+        {
+          field: "attribute_value",
+          name: "Value"
+        },
+        {
+          field: "",
+          name: "Action"
+        }
+      ],
+      actions: [
+        {
+          label: 'Edit',
+          action: 'edit_attribute',
+          command: (event) => handleActionClick('edit_attribute')
+        },
+        {
+          label: 'Delete',
+          action: 'delete_attribute',
+          command: (event) => handleActionClick('delete_attribute')
+        },
       ]
     }
   };
