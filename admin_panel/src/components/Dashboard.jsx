48ed5e5f59ce7a403627acf0be0cd518baec7465
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-16 11:35:51 +0530

    orders menu highlight issue fix

--- admin_panel/src/components/Dashboard.jsx
+++ admin_panel/src/components/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
 
     const goToOrders = () => {
         navigate('dataTable/orders');
-        dispatch(selectLeftMenu('Orders'));
+        dispatch(selectLeftMenu('orders'));
     }
 
     return(
