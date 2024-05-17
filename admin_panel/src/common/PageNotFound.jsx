c6b59357699b895ff06d4921777bb4212def1c03
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-17 19:39:27 +0530

    page not found illustration

--- admin_panel/src/common/PageNotFound.jsx
+++ admin_panel/src/common/PageNotFound.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function PageNotFound() {
-    return <h1 className="page_not_found tw-grid tw-place-items-center">Page Not Found !</h1>
+    return (
+        <div className="page_not_found  tw-grid tw-place-items-center tw-w-full tw-h-full">
+            <dotlottie-player src="https://lottie.host/d0f33808-9e32-4a6e-bea5-4226288ad9a9/h5XM4vhTod.json" background="transparent" speed="1" style={{width: "500px", height: "500px"}} loop autoplay></dotlottie-player>
+        </div>
+    )
 }
