ca4aad219f5a4317dab321ae4cec415468fa4d4c
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-05-20 19:31:39 +0530

    product details implement

--- admin_panel/src/components/ProductDetails.jsx
+++ admin_panel/src/components/ProductDetails.jsx
@@ -10,13 +10,15 @@ import { categories, subCategories } from "../../util";
 import { Button } from 'primereact/button';
 import { Chips } from 'primereact/chips';
 import { ColorPicker } from 'primereact/colorpicker';
-        
+import { Tag } from 'primereact/tag';
 import { Stepper } from 'primereact/stepper';
 import { StepperPanel } from 'primereact/stepperpanel';
 import { FileUpload } from 'primereact/fileupload';
 import { IoCloseOutline } from "react-icons/io5";
 import { ImImages } from "react-icons/im";
-
+import { DataView } from 'primereact/dataview';
+import { InputNumber } from 'primereact/inputnumber';
+import { Carousel } from 'primereact/carousel';
 
 export default function ProductDetails() {
 
@@ -31,6 +33,7 @@ export default function ProductDetails() {
   const [subCategoryOptions, setSubCategoryOptions] = useState([]);
   const colors = watch('colors', []); // Watch the colors array
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [products, setProducts] = useState([]);
   const stock_status_list = [
     { name: 'In Stock', code: 'I' },
     { name: 'Out of Stock', code: 'O' },
@@ -38,16 +41,18 @@ export default function ProductDetails() {
 
   const productSubmit = (data) => {
     console.log("data : ", data);
+    setProducts([...products, data]);
     const files = fileUploadRef.current.getFiles(); // Retrieve selected files
       console.log(files);
   }
 
   const addColor = () => {
+    let colorExist = false;
     let selected_color = getValues('colorPicker');
     let colors = getValues('colors');
     let color_name = getValues("color_name");
-    
-    if(selected_color && color_name && colors && !colors.includes(selected_color)){
+    colorExist = colors && colors.some((color) => color.color === selected_color);
+    if(selected_color && color_name && !colorExist){
       let color = {
         name : color_name,
         color : selected_color
@@ -116,7 +121,7 @@ export default function ProductDetails() {
 
   const imageList = (file, props) => {
     return (
-            <div className=" tw-flex tw-items-center tw-bg-[#ECECEC] tw-rounded-md tw-relative">
+            <div className=" tw-bg-[#ECECEC] tw-rounded-md tw-relative ">
                 <img alt={file.name} role="presentation" src={file.objectURL} />
                 <div className=' tw-absolute -tw-top-2 -tw-right-2 tw-bg-[#ECECEC] tw-rounded-full tw-w-6 tw-h-6 tw-text-center'>
                   <Button type="button" icon="pi pi-times" className=" grey_color tw-bg-transparent p-button-rounded p-button-danger ml-auto tw-w-4 tw-h-4 tw-border-0" onClick={() => onTemplateRemove(props.onRemove)} />
@@ -127,15 +132,83 @@ export default function ProductDetails() {
 
 const emptyTemplate = () => {
   return (
-      <div className="flex align-items-center flex-column">
-          <i className="pi pi-image mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
-          <span style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }} className="my-5">
+      <div className="tw-flex tw-items-center tw-gap-x-2">
+          <i className="pi pi-image mt-3 tw-rounded-full tw-text-xl " style={{ backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
+          <span style={{ color: 'var(--text-color-secondary)' }} className="my-5">
               Drag and Drop Image Here
           </span>
       </div>
   );
 };
 
+  const getSeverity = (product) => {
+    switch (product.stock_status) {
+        case 'INSTOCK':
+            return 'success';
+
+        case 'LOWSTOCK':
+            return 'warning';
+
+        case 'OUTOFSTOCK':
+            return 'danger';
+
+        default:
+            return null;
+    }
+  };
+
+  const productImageTemplate = (image) => {
+    return(
+      <div>
+          <img src={image.objectURL} alt={`product_image_${image.name}`} />
+      </div>
+    )
+  }
+
+  const listProducts = (items) => {
+      if (!items || items.length === 0) return null;
+
+      let list = items.map((product, index) => {
+          return listProductsUI(product, index);
+      });
+
+      return <div className="grid grid-nogutter">{list}</div>;
+  };
+
+  const listProductsUI = (product, index) => {
+      return (
+        <div className=' tw-flex' key={`product_${product.title}_${index}`}>
+            <Carousel value={product.image} numVisible={3} numScroll={3} itemTemplate={productImageTemplate} />
+            <div>
+              Quantity : {product.quantity}<br />
+              Size : {product?.sizes?.map((v,i) => {
+                return(
+                  <>
+                    <span key={`product_size_${v}`}>{v}</span>
+                  </>
+                )
+              })}
+            </div>
+            <div>
+                  {
+                    product?.colors?.map((val, index) => {
+                      return(
+                        <>
+                          <div key={`product_color_list_${val}`} className=' tw-w-5 tw-h-5 tw-mr-3 tw-rounded-full'
+                              style={{
+                                backgroundColor: `#${val.color}`,
+                              }}
+                            />
+                            #{val.name}
+                        </>
+                      )
+                    })
+                  }
+                  <div>${product.price}</div>
+            </div>
+        </div>
+      );
+  }
 
   return (
     <div className='product_details  tw-p-10'>
@@ -190,46 +263,21 @@ const emptyTemplate = () => {
                       <div  className=' tw-grid tw-gap-4'>
                         <div className=' tw-grid'>
                           <label htmlFor="">Images</label>
-                          <FileUpload ref={fileUploadRef} multiple name="demo[]" url="/api/upload" accept="image/*" maxFileSize={1000000} itemTemplate={imageList} emptyTemplate={emptyTemplate} chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions}/>
-                          {/* <label htmlFor="fileUpload" style={{ display: 'inline-block' }}> */}
-                          {/* <Button type='button'
-                          label="Upload Image"
-                          icon="pi pi-upload"
-                          
-                        /> */}
-                        {/* </label> */}
-                        {/* <FileUpload
-                        id="fileUpload"
-                          ref={fileUploadRef}
-                          name="demo[]"
-                          accept="image/*"
-                          maxFileSize={1000000}
-                          customUpload
-                          auto
-                        /> */}
-                        {/* {uploadedImage && (
-                          <div className="p-d-flex p-ai-center p-jc-between" style={{ marginTop: '10px' }}>
-                            <img src={uploadedImage} alt="Uploaded" style={{ width: '100px' }} />
-                            <Button icon="pi pi-times" className="p-button-danger" onClick={onRemove} />
-                          </div>
-                        )} */}
-                          {/* <input type='file' name="" id="" className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/> */}
-                          {/* <FileUpload name="demo[]" url={'/api/upload'}  customUpload accept="image/*" maxFileSize={1000000} auto
-      itemTemplate={itemTemplate}
-      chooseOptions={chooseOptions}
-      uploadOptions={uploadOptions}
-      cancelOptions={cancelOptions}/> */}
-                          {/* <FileUpload ref={fileUploadRef} name="demo[]" url="/api/upload" multiple accept="image/*" maxFileSize={1000000}
-                onUpload={onTemplateUpload} onSelect={onTemplateSelect} onError={onTemplateClear} onClear={onTemplateClear}
-                headerTemplate={headerTemplate} itemTemplate={itemTemplate} emptyTemplate={emptyTemplate}
-                chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions} /> */}
+                          <Controller name='image[]' control={control} defaultValue={[]} render={({field}) => (
+                            <FileUpload ref={fileUploadRef} name={field.name} multiple  url="/api/upload" accept="image/*" maxFileSize={1000000} itemTemplate={imageList} emptyTemplate={emptyTemplate} chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions} onSelect={(e) => field.onChange(e.files)}
+                            onRemove={(e) => {
+                              const updatedFiles = field.value.filter(file => !e.files.includes(file));
+                              field.onChange(updatedFiles);
+                            }}
+                            onClear={() => field.onChange([])}/>
+                          )} />
                         </div>
                         <div className=' tw-grid'>
                             <label htmlFor="">Colors</label>
                             <Controller
                                 name="colorPicker"
                                 control={control}
-                                defaultValue={null}
+                                defaultValue={"ff0000"}
                                 render={({ field }) => (
                                   <ColorPicker
                                     value={field.value}
@@ -244,24 +292,20 @@ const emptyTemplate = () => {
                             <input type="text" {...register("color_name")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
                         </div>
                         <Button type='button' label="Add Color" onClick={addColor} className='tw-h-10  tw-w-32 shopper-bgcolor tw-text-white tw-rounded-md  tw-text-sm tw-font-medium' />
-                        <div>
-                            <ul>
+                        {colors.length > 0 && <div>
+                            <ul className=' tw-grid tw-gap-2'>
                                 {getValues('colors').map((val, index) => (
                                     <li key={`color_${val.color}`} style={{ display: 'flex', alignItems: 'center' }}>
-                                        <div
+                                        <div className=' tw-w-5 tw-h-5 tw-mr-3 tw-rounded-full'
                                           style={{
                                             backgroundColor: `#${val.color}`,
-                                            width: '20px',
-                                            height: '20px',
-                                            marginRight: '10px',
-                                            border: '1px solid #000',
                                           }}
                                         />
                                         #{val.name}
                                     </li>
                                 ))}
                             </ul>
-                        </div>
+                        </div>}
                         <div className=' tw-grid'>
                             <label htmlFor="">Sizes</label>
                             <Controller
@@ -279,7 +323,15 @@ const emptyTemplate = () => {
                         </div>
                         <div className=' tw-grid'>
                           <label htmlFor="">Price</label>
-                          <input type="text" name="" id="" {...register("price")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
+                          <Controller control={control} name='price' defaultValue={0.00} render={({field}) => (
+                              <InputNumber value={field.value} onValueChange={(e) => field.onChange(e.value)} mode="currency" currency="USD" locale="en-US" />
+                          )}/>
+                        </div>
+                        <div className=' tw-grid'>
+                          <label htmlFor="">Quantity</label>
+                          <Controller control={control} name='quantity' defaultValue={0.00} render={({field}) => (
+                              <InputNumber value={field.value} onValueChange={(e) => field.onChange(e.value)} />
+                          )}/>
                         </div>
                         <div className=' tw-flex tw-justify-between'>
                             <Button type='button' label="Back" className=' tw-mt-20 tw-h-10  tw-w-32 shopper-bgcolor tw-text-white tw-rounded-md  tw-text-sm tw-font-medium' onClick={() => stepperRef.current.prevCallback()}/>
@@ -288,118 +340,12 @@ const emptyTemplate = () => {
                       </div>
                     </StepperPanel>
               </Stepper>
-            </form>
-          {/* <form className='tw-p-10' onSubmit={handleSubmit(productSubmit)}>
-            <div className=' tw-grid grid-col' style={{gridTemplateColumns: '50% 50%'}}>
-              <div className=' tw-grid tw-gap-3'>
-                <div className=' tw-grid'>
-                    <label htmlFor="">Title</label>
-                    <input type="text" name="" id="" {...register("title")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Price</label>
-                    <input type="text" name="" id="" {...register("price")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Category</label>
-                    <Controller name='category' control={control} render={({field}) => (
-                          <Dropdown {...field}  options={categories} optionLabel="value" placeholder="Select category" className="  tw-border-2 tw-h-11 tw-border-[#E6E7E8] tw-rounded-lg" />
-                      )} 
-                    />
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Sub Category</label>
-                    <Controller name='sub_category' control={control} render={({field}) => (
-                          <Dropdown {...field}  options={subCategoryOptions} optionLabel="value" placeholder="Select sub-category" className="  tw-border-2 tw-h-11 tw-border-[#E6E7E8] tw-rounded-lg" />
-                      )} 
-                    />
-                  </div>
-                  
-                  <div className=' tw-grid'>
-                      <label htmlFor="">Stock status</label>
-                      <Controller name='stock_status' control={control} render={({field}) => (
-                          <Dropdown {...field}  options={stock_status_list} optionLabel="name" placeholder="Select stock status" className="  tw-border-2 tw-h-11 tw-border-[#E6E7E8] tw-rounded-lg" />
-                        )} 
-                      />
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Available quantity</label>
-                    <input type="number" name="" id="" {...register("quantity")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
+          </form>
+          { products && 
+              <div className=' w-full '>
+                <DataView value={products} listTemplate={listProducts} />
               </div>
-              <div className=' tw-grid tw-gap-3'>
-                  <div className=' tw-grid'>
-                      <label htmlFor="">Slug</label>
-                      <input type="text" name="" id="" {...register("slug")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">SKU</label>
-                    <input type="text" {...register("sku")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Images</label>
-                    <input type='file' name="" id="" className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Colors</label>
-                    <Controller
-                        name="colorPicker"
-                        control={control}
-                        defaultValue={null}
-                        render={({ field }) => (
-                          <ColorPicker
-                            value={field.value}
-                            onChange={(e) => field.onChange(e.value)}
-                            className={`  tw-rounded-lg`}
-                          />
-                        )}
-                      />
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Color name</label>
-                    <input type="text" {...register("color_name")} className='tw-border-2 tw-border-[#E6E7E8] tw-rounded-lg'/>
-                  </div>
-                  <button type='button' onClick={addColor}>Add Color</button>
-                  <div>
-                    <ul>
-                      {getValues('colors').map((val, index) => (
-                        <li key={`color_${val.color}`} style={{ display: 'flex', alignItems: 'center' }}>
-                          <div
-                            style={{
-                              backgroundColor: `#${val.color}`,
-                              width: '20px',
-                              height: '20px',
-                              marginRight: '10px',
-                              border: '1px solid #000',
-                            }}
-                          />
-                          #{val.name}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className=' tw-grid'>
-                    <label htmlFor="">Sizes</label>
-                    <Controller
-                        name="sizes"
-                        control={control}
-                        defaultValue={[]}
-                        render={({ field }) => (
-                          <Chips
-                            value={field.value}
-                            onChange={(e) => field.onChange(e.value)}
-                            className={`  tw-rounded-lg`}
-                          />
-                        )}
-                      />
-                  </div>
-              </div> 
-            </div>
-            <div className=' tw-mt-10'>
-              <Editor className=' tw-h-40 tw-max-w-[675px]' headerTemplate={renderEditorHeader}/>
-            </div>
-            <Button type='submit' label="Add Product" className=' tw-mt-20 tw-h-10  tw-w-32 shopper-bgcolor tw-text-white tw-rounded-md  tw-text-sm tw-font-medium' />
-          </form> */}
+          }
         </div>
       </div>
     </div>
