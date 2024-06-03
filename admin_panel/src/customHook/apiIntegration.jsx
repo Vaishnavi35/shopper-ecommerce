91a731f4f3b62f2c85c44dc4a686cc8ffb75ef9d
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-06-03 17:50:00 +0530

    api integration change

--- admin_panel/src/customHook/apiIntegration.jsx
+++ admin_panel/src/customHook/apiIntegration.jsx
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
-export const apiIntegration = (props) => {
+export const apiIntegration = () => {
     const host = process.env.REACT_APP_API_URL || "http://localhost:3000";
-    const HTTP_TYPE = props.type || 'GET';
-    const apiURL = props.url;
-    const params = props.params || null;
     console.log("host : ",host);
-    console.log("HTTP_TYPE : ",HTTP_TYPE);
-
-    console.log("apiURL : ",apiURL);
-    console.log("params : ",params);
+    
 
     const [data,setData] = useState(null);
     const [error,setError] =  useState(null);
     const [loading,setLoading] =  useState(true);
 
-    const fetchData = async () => {
+    const fetchData = async (val) => {
+        let HTTP_TYPE = val.httpType || 'GET';
+        let apiURL = val.apiURL;
+        let params = val.params || null;
+
+        console.log("HTTP_TYPE : ",HTTP_TYPE);
+        console.log("apiURL : ",apiURL);
+        console.log("params : ",params);
         try {
             let response;
             if(HTTP_TYPE == 'POST'){
-                response = await axios.post(`${host}${apiURL}`, {
-                    dataParm : params
+                response = await axios.post(`${host}${apiURL}`, params, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 })
             }else{
                 response = await axios.get(`${host}${apiURL}`);
@@ -38,29 +41,14 @@ export const apiIntegration = (props) => {
         }
     }
 
-    useEffect(() => {
+    // useEffect(() => {
         
-        fetchData();
-        // await axios.get(props.url)
-        // .then((response) => 
-        //     {
-        //         setLoading(false); 
-        //         setData(response.data);
-        //         console.log("data : ", response);
-        //     }
-        // )
-        // .catch((error) => 
-        //     {
-        //         setLoading(false); 
-        //         setError(error);
-        //         console.error("error : ",error);
-        //     }
-        // )
-    },[apiURL,params,HTTP_TYPE])
+    //     fetchData();
+    // },[apiURL,params,HTTP_TYPE])
 
     if (loading) {
         console.log("data fetching ...");
     }
 
-  return {data, loading, error};
+  return {data, loading, error, fetchData};
 }
